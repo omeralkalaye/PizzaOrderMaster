0e40f26a1206886639cd4404ec6f3c272ddbe8a4
ygmfffmt2b <40076038-ygmfffmt2b@users.noreply.replit.com>
2025-02-27 23:19:56 +0000

    Restored to '25abd53e3b96f0daa9a8330290a4964ad408a84c'
    Replit-Restored-To:25abd53e3b96f0daa9a8330290a4964ad408a84c

--- client/src/types/cart.ts
+++
@@ -1,7 +0,0 @@
-import { Pizza, PizzaOrderItem } from "@shared/schema";
-
-export interface CartItem extends PizzaOrderItem {
-  pizza: Pizza;
-  isCreamSauce?: boolean;
-  isVeganCheese?: boolean;
-}
