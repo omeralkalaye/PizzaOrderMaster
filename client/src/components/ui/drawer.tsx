74b4c834b93478ef77b3586716a05cccbed86d9f
ygmfffmt2b <40076038-ygmfffmt2b@users.noreply.replit.com>
2025-02-28 00:11:45 +0000

    Fix: Adjust drawer opening direction to left
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/c6e031c6-3de9-4b27-b27b-7cefb4a6165e/e66206cc-bf9b-4170-a242-0ee3500b936b.jpg

--- client/src/components/ui/drawer.tsx
+++ client/src/components/ui/drawer.tsx
@@ -41,7 +41,7 @@ const DrawerContent = React.forwardRef<
     <DrawerPrimitive.Content
       ref={ref}
       className={cn(
-        "fixed inset-y-0 right-0 z-50 h-full w-3/4 border-l bg-background transition-transform duration-300 data-[state=closed]:translate-x-full data-[state=open]:translate-x-0 sm:max-w-sm",
+        "fixed inset-y-0 left-0 z-50 h-full w-3/4 border-r bg-background transition-transform duration-300 data-[state=closed]:-translate-x-full data-[state=open]:translate-x-0 sm:max-w-sm",
         className
       )}
       {...props}
