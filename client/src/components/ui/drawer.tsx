2cda539909b86567bff6a675f0d3f71103229d3b
ygmfffmt2b <40076038-ygmfffmt2b@users.noreply.replit.com>
2025-02-28 00:08:39 +0000

    Adjust side menu to open from the left.
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/c6e031c6-3de9-4b27-b27b-7cefb4a6165e/e16f2637-dbbc-40fb-b116-89bff4acba1e.jpg

--- client/src/components/ui/drawer.tsx
+++ client/src/components/ui/drawer.tsx
@@ -41,7 +41,7 @@ const DrawerContent = React.forwardRef<
     <DrawerPrimitive.Content
       ref={ref}
       className={cn(
-        "fixed inset-x-0 bottom-0 z-50 mt-24 flex h-auto flex-col rounded-t-[10px] border bg-background",
+        "fixed inset-y-0 left-0 z-50 h-full w-3/4 border-r bg-background transition-all sm:max-w-sm",
         className
       )}
       {...props}
@@ -58,7 +58,7 @@ const DrawerHeader = ({
   ...props
 }: React.HTMLAttributes<HTMLDivElement>) => (
   <div
-    className={cn("grid gap-1.5 p-4 text-center sm:text-left", className)}
+    className={cn("grid gap-1.5 p-4 text-center sm:text-right", className)}
     {...props}
   />
 )
